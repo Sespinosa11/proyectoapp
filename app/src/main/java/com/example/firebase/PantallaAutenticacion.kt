7065d70b6086d0f7c2fa8b1972b24770cd960a91
SergioAldana <saaldana2@misena.edu.co>
2022-05-19 13:38:27 -0500

    Se modifica el archivo PantallaAutenticacion principalmente

--- app/src/main/java/com/example/firebase/PantallaAutenticacion.kt
+++ app/src/main/java/com/example/firebase/PantallaAutenticacion.kt
@@ -18,24 +18,27 @@ class PantallaAutenticacion : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_autenticacion)
 
-        var textoUno: TextView =findViewById(R.id.textView2)
-        val instanciaFirestore = FirebaseFirestore.getInstance();
+        var textoUno: TextView = findViewById(R.id.textView2)
+        val instanciaFirestore = FirebaseFirestore.getInstance()
 
-        instanciaFirestore.collection("prueba").add(mapOf("cedula" to 1003522752, "nombre" to "sergio"))
+        instanciaFirestore.collection("prueba")
+            .add(mapOf("cedula" to 1003522752, "nombre" to "sergio"))
             .addOnSuccessListener {
-                instanciaFirestore.collection("prueba").document(it.id).update(mapOf("idDoc" to it.id))
+                instanciaFirestore.collection("prueba").document(it.id)
+                    .update(mapOf("idDoc" to it.id))
             }.addOnFailureListener {
-                Toast.makeText(this,it.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
             }
 
-        instanciaFirestore.collection("prueba").whereEqualTo("nombre", "FFF").get().addOnSuccessListener {
-            it.documents.forEach{
-                Log.i("items",it.data.toString())
-                Log.i("dato_ejemplo",it?.data?.get("cedula").toString())
-                textoUno.text=it?.data?.get("cedula").toString()
+        instanciaFirestore.collection("prueba").whereEqualTo("nombre", "FFF").get()
+            .addOnSuccessListener {
+                it.documents.forEach {
+                    Log.i("items", it.data.toString())
+                    Log.i("dato_ejemplo", it?.data?.get("cedula").toString())
+                    textoUno.text = it?.data?.get("cedula").toString()
 
+                }
             }
-        }
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
         val navigationView: NavigationView = findViewById(R.id.navigationView)
@@ -47,29 +50,51 @@ class PantallaAutenticacion : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navigationView.setNavigationItemSelectedListener {
-            when(it.itemId) {
 
-                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
-                R.id.nav_message -> Toast.makeText(applicationContext, "Clicked Message", Toast.LENGTH_SHORT).show()
-                R.id.nav_sync -> Toast.makeText(applicationContext, "Clicked Sync", Toast.LENGTH_SHORT).show()
-                R.id.nav_trash -> Toast.makeText(applicationContext, "Clicked Trash", Toast.LENGTH_SHORT).show()
-                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
-                R.id.nav_login -> Toast.makeText(applicationContext, "Clicked Login", Toast.LENGTH_SHORT).show()
-                R.id.nav_share -> Toast.makeText(applicationContext, "Clicked Share", Toast.LENGTH_SHORT).show()
-                R.id.nav_rate_us -> Toast.makeText(applicationContext, "Clicked Rate us", Toast.LENGTH_SHORT).show()
+            when (it.itemId) {
+                R.id.nav_home -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Inicio",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_lacteos -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Lacteos",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_frutas -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Frutas",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_vegetales -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Vegetales",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_carnes -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Carnes",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_settings -> Toast.makeText(
+                    applicationContext,
+                    "Cliqueado Configuración",
+                    Toast.LENGTH_SHORT
+                ).show()
 
             }
-
             true
+
         }
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         if (toggle.onOptionsItemSelected(item)) {
             return true
         }
-
         return super.onOptionsItemSelected(item)
     }
 }
